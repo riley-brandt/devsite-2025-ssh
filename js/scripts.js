746dc1d9d9a5e059f5260b8f5cd995d961c6029b
riley-brandt <rileyb3@proton.me>
2025-03-22 21:21:08 -0600

    CSS, HTML, and JS: Struggling to make a proper responsive hamburger menu from scratch.

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,12 @@
 /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
-function myFunction() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
-  } else {
-    x.className = "topnav";
-  }
-}
+// function menuButtonfunc() {
+//   var x = document.getElementById("myTopnav");
+//   if (x.className === "topnav") {
+//     x.className += " responsive";
+//   } else {
+//     x.className = "topnav";
+//   }
+// }
 
 // =========== NEW RESPONSIVE NAV BAR =============
 
