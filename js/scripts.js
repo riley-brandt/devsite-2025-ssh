830e9dcfc2a3661e281330730b5f7e676ef38a7a
riley-brandt <rileyb3@proton.me>
2025-03-25 19:36:54 -0600

    HTML/CSS/JS: Finshed all basic funcationality of the repsonsive navigation bar. Added a second menu, styled for mobile, hidden, and then showen with a simple JS script.

--- js/scripts.js
+++ js/scripts.js
@@ -1,39 +1,11 @@
-/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
-// function menuButtonfunc() {
-//   var x = document.getElementById("myTopnav");
-//   if (x.className === "topnav") {
-//     x.className += " responsive";
-//   } else {
-//     x.className = "topnav";
-//   }
-// }
-
-// ============== Test Nav Bar ===============
-
-// document.getElementById("nav-toggle").addEventListener("click", function () {
-//   document.querySelector(".nav").classList.toggle("active");
-// });
-
 // =========== NEW RESPONSIVE NAV BAR =============
 
-const theBody = document.querySelector("body");
-const openNav = document.querySelector(".menu-bar button");
-const closeNav = document.querySelector(".close-nav button");
-const Navbar = document.querySelector(".navbar");
-
-// Hiding body scroll when mobile navigation menu opens
-function bodyScroll() {
-  if (Navbar.classList.contains("show")) {
-    theBody.classList.add("hide-scroll");
-  } else if (theBody.classList.contains("hide-scroll")) {
-    theBody.classList.remove("hide-scroll");
-  }
+function showSidebar() {
+  const sidebar = document.querySelector(".sidebar");
+  sidebar.style.display = "flex";
 }
 
-function showHide() {
-  Navbar.classList.toggle("show");
-  bodyScroll();
+function hideSidebar() {
+  const sidebar = document.querySelector(".sidebar");
+  sidebar.style.display = "none";
 }
-
-openNav.onclick = showHide;
-closeNav.onclick = showHide;
